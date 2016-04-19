d292c023467fd02acf57e45996c7d861cae3a7cc
Niki_Home-PC\Niki <NikiHerl@gmail.com>
2016-04-19 18:24:58 +0200

    IT'S BREATHING... AND SAVING... AND LOADING... yeah

--- JS/logic.js
+++ JS/logic.js
@@ -7,6 +7,8 @@ var currentSite;
 //the HTMLCollection of all "sites" (aka pseudo-site divs)
 var siteArray = [];
 
+var numberOfVisibleNotes = 30;
+
 //really, this should be what calls initSites, but since I don't want to introduce tons of code to make sure it is called at some point even if the app isn't run in Cordova/PhoneGap, I'm not going to do it. If/When I want to do it, this site knows how http://stackoverflow.com/questions/12576062/jquery-document-ready-vs-phonegap-deviceready
 function onDeviceReady() {
     console.log("device is ready, whatever that means");
@@ -20,6 +22,7 @@ function initSites() {
         siteArray[i].style.display = "none";
     }
     switchVisibleSite("homepage");
+    initViewNote(numberOfVisibleNotes);
     console.log("initSites() has been called");
 }
 
@@ -53,39 +56,70 @@ function openNote() {
         console.log("there was a note in the way, safed and deleted it")
     }
 
-    if (document.getElementById("note").getAttribute("localStorageId") == "") {
+    if (document.getElementById("newNote").getAttribute("localStorageId") == "") {
         //I REALLY should make this check if the number has already been used but I'm way in over my head anyway... and tbh, this is funny
-        document.getElementById("note").setAttribute("localStorageId", getRandomId());
+        document.getElementById("newNote").setAttribute("localStorageId", getRandomId());
     }
-    
-    console.log("the note-div's 'localStorageId' is: " + document.getElementById("note").getAttribute("localStorageId"));
-    
+
+    console.log("the note-div's 'localStorageId' is: " + document.getElementById("newNote").getAttribute("localStorageId"));
+
     var titleTextArea = document.createElement("textarea");
     titleTextArea.setAttribute("id", "newNoteTitle");
-    titleTextArea.setAttribute("class", "textArea");
+    titleTextArea.setAttribute("class", "textArea title");
     titleTextArea.setAttribute("maxlength", "500");
     titleTextArea.setAttribute("rows", "2");
     titleTextArea.setAttribute("placeholder", "Add an Title");
 
     var contentTextArea = document.createElement("textarea");
     contentTextArea.setAttribute("id", "newNoteContent");
-    contentTextArea.setAttribute("class", "textArea");
+    contentTextArea.setAttribute("class", "textArea content");
     contentTextArea.setAttribute("maxlength", "10000");
     contentTextArea.setAttribute("rows", "10");
     contentTextArea.setAttribute("placeholder", "Main Content Here");
 
-    document.getElementById("note").appendChild(titleTextArea);
+    document.getElementById("newNote").appendChild(titleTextArea);
     document.getElementById("newNoteTitle").focus();
-    document.getElementById("note").appendChild(contentTextArea);
-    
-    console.log(document.getElementById("note").getAttribute("localStorageId"));
+    document.getElementById("newNote").appendChild(contentTextArea);
+}
+
+function openExistingNote(title, content, localStorageId, targetDivId) {
+    var targetDiv = document.getElementById(targetDivId);
+
+    //make sure there's nothing there
+    while (targetDiv.firstChild) {
+        targetDiv.removeChild(targetDiv.firstChild);
+    }
+
+    targetDiv.setAttribute("localStorageId", localStorageId);
+
+
+    console.log("the note-div's 'localStorageId' is: " + localStorageId);
+
+    var titleTextArea = document.createElement("textarea");
+    titleTextArea.setAttribute("id", targetDivId + "Title");
+    titleTextArea.setAttribute("class", "textArea title");
+    titleTextArea.setAttribute("maxlength", "500");
+    titleTextArea.setAttribute("rows", "2");
+    titleTextArea.setAttribute("placeholder", "Add an Title");
+    titleTextArea.value = title;
+
+    var contentTextArea = document.createElement("textarea");
+    contentTextArea.setAttribute("id", targetDivId + "Content");
+    contentTextArea.setAttribute("class", "textArea content");
+    contentTextArea.setAttribute("maxlength", "10000");
+    contentTextArea.setAttribute("rows", "10");
+    contentTextArea.setAttribute("placeholder", "Main Content Here");
+    contentTextArea.value = content;
+
+    targetDiv.appendChild(titleTextArea);
+    targetDiv.appendChild(contentTextArea);
 }
 
 function safeNewNote() {
     var titleElement = document.getElementById("newNoteTitle");
-    var title = titleElement.value.trim();
+    var title = titleElement.value;
     console.log(title);
-    var content = document.getElementById("newNoteContent").value.trim();
+    var content = document.getElementById("newNoteContent").value;
 
     //I need to have titleLength always be of a certain length so I now weird stuff happens when opening the note (reading the string) later on
     if (title.length > 0 || content.length > 0) {
@@ -102,7 +136,7 @@ function safeNewNote() {
             return title.length;
         })();
 
-        var localStorageName = document.getElementById("note").getAttribute("localStorageId");
+        var localStorageName = document.getElementById("newNote").getAttribute("localStorageId");
         window.localStorage.setItem(localStorageName, titleLength + title + content);
         console.log("safeNote() was called and localStorageName is: " + localStorageName);
     } else {
@@ -117,10 +151,49 @@ function safeNewNote() {
 }
 
 function deleteNote() {
-    var noteDiv = document.getElementById("note");
+    var noteDiv = document.getElementById("newNote");
     noteDiv.removeChild(document.getElementById("newNoteTitle"));
     noteDiv.removeChild(document.getElementById("newNoteContent"));
-    document.getElementById("note").dataset.localStorageId = "";
+    document.getElementById("newNote").setAttribute("localStorageId", "");
+}
+
+function openNoteByName(localStorageName, targetDivId) {
+    noteReader(localStorageName, targetDivId);
+}
+
+function openNoteByLocation(localStorageIndex, targetDivId) {
+    if (window.localStorage.key(localStorageIndex)) {
+        noteReader(window.localStorage.key(localStorageIndex), targetDivId);
+    }
+}
+
+//actually makes the conversion from one long string with all the data (title, content, metadata) to the note and immediately puts it in the target Div (has to be a note thingy)
+function noteReader(localStorageName, targetDivId) {
+    var localStorageValue = window.localStorage.getItem(localStorageName)
+    var titleLength = +localStorageValue.substring(0, 3);
+    var title = localStorageValue.substring(3, 3 + titleLength);
+    var content = localStorageValue.substring(3 + titleLength);
+    openExistingNote(title, content, localStorageName, targetDivId)
+}
+
+function initViewNote(numberOfNotes) {
+    var noteViewContainer = document.getElementById("viewNote");
+    var divToAttach;
+    for (var i = 0; i < numberOfNotes; i++) {
+        divToAttach = document.createElement("div");
+        divToAttach.setAttribute("id", "noteView" + i);
+        divToAttach.setAttribute("localStorageId", "");
+        noteViewContainer.appendChild(divToAttach);
+    }
+    updateViewNote(numberOfNotes);
+}
+
+function updateViewNote(numberOfNotes) {
+    for (var i = 0; i < numberOfNotes; i++) {
+        var targetDivId = "noteView" + i;
+        openNoteByLocation(i, targetDivId);
+    }
+    console.log("omg is dis real lyfe?");
 }
 
 initSites();
